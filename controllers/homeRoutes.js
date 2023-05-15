01a54ec5630fc0e146fb7e3f7c22535af51fca1c
mrobles-dev <miltonjrobles1996@gmail.com>
2023-05-15 14:29:45 -0500

    login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,4 +19,39 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
+
+router.get("/tracker", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+
+
+
+
+
 module.exports = router;
