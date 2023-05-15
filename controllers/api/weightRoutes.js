5589467cbc1dc9f22f4e3ce2f9668205eef102aa
marvin blunck <marvinblunck@gmail.com>
2023-05-15 13:50:48 -0700

    finished weight routing

--- controllers/api/weightRoutes.js
+++ controllers/api/weightRoutes.js
@@ -18,7 +18,42 @@ router.post('/', withAuth, async (req, res) => {
     }
   });
 
-  router.get('/weight/:id', withAuth, async (req, res) => {
-    const userId = req.params.id;
+  router.get('/', withAuth, async (req, res) => {
+    try {
+      const weightId = req.session.user_id;
+      const weightData = await Weight.findAll({
+        where: {
+          user_id: userId,
+        }
+      });
+      
+      res.status(200).json(weightData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
   });
+
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const weightId = req.params.id;
+      const userId = req.session.user_id;
+  
+      const deletedWeightIntake = await Weight.destroy({
+        where: {
+          id: weightId,
+          user_id: userId
+        }
+      });
+  
+      if (!deletedWeightIntake) {
+        res.status(404).json({ message: 'No weight intake entry found with this id!' });
+        return;
+      }
+  
+      res.status(200).json({ message: 'Weight intake entry deleted successfully!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
   module.exports = router;
