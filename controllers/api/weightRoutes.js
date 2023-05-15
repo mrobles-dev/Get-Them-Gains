7d2b7506a3a54d519b2962315ada758964e97dd1
marvin blunck <marvinblunck@gmail.com>
2023-05-15 11:16:24 -0700

    started models and route

--- controllers/api/weightRoutes.js
+++ controllers/api/weightRoutes.js
@@ -4,3 +4,21 @@ const withAuth = require('../../utils/auth');
 
 const moment = require('moment');
 const { Op } = require('sequelize');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newWeight = await Weight.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newWeight);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.get('/weight/:id', withAuth, async (req, res) => {
+    const userId = req.params.id;
+  });
+  module.exports = router;
