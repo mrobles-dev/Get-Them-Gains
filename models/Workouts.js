475ac46736bf6f637465b2d9c7ba6a8f13cf9d75
mrobles-dev <miltonjrobles1996@gmail.com>
2023-05-16 13:26:27 -0500

    workout models

---
+++ models/Workouts.js
@@ -0,0 +1,42 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+const { Workouts } = require('.');
+
+class Workouts extends Model{}
+
+
+Workouts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      // allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "sleep",
+  }
+);
+
+
+module.exports = Workouts;
