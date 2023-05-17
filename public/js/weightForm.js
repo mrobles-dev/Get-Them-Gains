e99b66acb3f381d9cc97aac7da2a2142cdd2c789
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-17 13:53:09 -0500

    weight form works and data shows in db

---
+++ public/js/weightForm.js
@@ -0,0 +1,31 @@
+const weightFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the sleep form
+  const date = document.querySelector('#date').value.trim();
+  const currentWeight = document.querySelector('#currentWeight').value.trim();
+  const goalWeight = document.querySelector('#goalWeight').value.trim();
+
+
+  if (date && currentWeight && goalWeight) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/weight', {
+      method: 'POST',
+      body: JSON.stringify({ date, currentWeight, goalWeight }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // If successful, show the message below
+      console.log('Weight data submitted successfully!');
+    } else {
+      // If there is an error, display the error message
+      const errorData = await response.json();
+      console.error(errorData);
+    }
+  }
+};
+
+document
+  .querySelector('.weight-form')
+  .addEventListener('submit', weightFormHandler);
