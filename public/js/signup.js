973e0addaefd193695316a3c4d09014fc95b68a5
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-16 21:58:48 -0500

    signup works

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,11 +4,13 @@ const signupFormHandler = async (event) => {
   const name = document.querySelector('#name').value.trim();
   const email = document.querySelector('#email').value.trim();
   const password = document.querySelector('#password').value.trim();
+  const startingWeight = document.querySelector('#startingWeight').value.trim();
+
 
   if (name && email && password) {
-    const response = await fetch('/api/users', {
+    const response = await fetch('/api/users/signup', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ name, email, password, startingWeight }),
       headers: { 'Content-Type': 'application/json' },
     });
 
