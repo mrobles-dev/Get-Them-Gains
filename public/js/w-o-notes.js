a6609d5ea98eeb0183c30cf21a1689d72dea0781
mrobles-dev <miltonjrobles1996@gmail.com>
2023-05-18 11:47:48 -0500

    working on rendering notes

--- public/js/w-o-notes.js
+++ public/js/w-o-notes.js
@@ -1,105 +1,28 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
+const workoutHandler = async (event) =>{
+    event.preventDefault();
 
-console.log("ding")
+const title = document.querySelector('#note-title').value.trim();
+const text = document.querySelector('#note-textarea').value.trim();
 
-
-
-
-if (window.location.pathname === "/workouts") {
-  noteTitle = document.querySelector(".note-title");
-  noteText = document.querySelector(".note-textarea");
-  saveNoteBtn = document.querySelector(".save-note");
-  newNoteBtn = document.querySelector(".new-note");
-  noteList = document.querySelectorAll(".list-container .list-group");
+if (title && text) {
+    try{
+    const response = await fetch("/api/workouts", {
+        method: 'POST',
+        body: JSON.stringify({title, text}),
+        headers: {"Content-Type" : "application/json"},
+    });
+if (response.ok){
+    console.log("you got it dude!");
+} else {
+    const err = await response.json();
+    console.error(err)
+}
+} catch (error) {
+    console.error('erorr', error)
 }
 
-// Show an element
-const show = (elem) => {
-  elem.style.display = "block";
-};
-
-
-// activeNote is used to keep track of the note in the textarea
-let activeNote = {};
-
-
-
-const handleNoteSave = () => {
-  const newNote = {
-    title: noteTitle.value,
-    text: noteText.value,
-  };
-  saveNote(newNote).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
-};
-
-
-
-// Sets the activeNote and displays it
-const handleNoteView = (e) => {
-  e.preventDefault();
-  activeNote = JSON.parse(e.target.parentElement.getAttribute("data-note"));
-  renderActiveNote();
-};
-
-// Sets the activeNote to and empty object and allows the user to enter a new note
-const handleNewNoteView = (e) => {
-  activeNote = {};
-  renderActiveNote();
-};
-
-// Render the list of note titles
-const renderNoteList = async (notes) => {
-  let jsonNotes = await notes.json();
-  if (window.location.pathname === "/workouts") {
-    noteList.forEach((el) => (el.innerHTML = ""));
-  }
-
-  let noteListItems = [];
-
-  // Returns HTML element with or without a delete button
-  const createLi = (text, delBtn = true) => {
-    const liEl = document.createElement("li");
-    liEl.classList.add("list-group-item");
-
-    const spanEl = document.createElement("span");
-    spanEl.classList.add("list-item-title");
-    spanEl.innerText = text;
-    spanEl.addEventListener("click", handleNoteView);
-
-    liEl.append(spanEl);
-
-    return liEl;
-  };
-
-  if (jsonNotes.length === 0) {
-    noteListItems.push(createLi("No saved Notes", false));
-  }
-
-  jsonNotes.forEach((note) => {
-    const li = createLi(note.title);
-    li.dataset.note = JSON.stringify(note);
-
-    noteListItems.push(li);
-  });
-
-  if (window.location.pathname === "/workouts") {
-    noteListItems.forEach((note) => noteList[0].append(note));
-  }
-};
-
-// Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
-
-if (window.location.pathname === "/workouts") {
-  saveNoteBtn.addEventListener("click");
-  newNoteBtn.addEventListener("click" );
 }
+};
 
-getAndRenderNotes()
+
+document.querySelector(".save-note").addEventListener("submit", workoutHandler);
