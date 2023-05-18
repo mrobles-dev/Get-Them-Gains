4d10eecf30d7f037d149dc6c2bbabf34ecfacb31
kevincallaghan <kevincallaghan5@yahoo.com>
2023-05-18 18:48:58 -0500

    nav change

--- public/js/goals.js
+++ public/js/goals.js
@@ -1,10 +1,3 @@
-window.onload = function() {
-  setTimeout(function() {
-    var delayedImage = document.querySelector('.delayed-image');
-    delayedImage.classList.add('fade-in');
-  }, 300);
-};
-
 // Source: Beyond Fireship Youtube Video "Subtle, yet Beautiful Scroll Animations" link: https://www.youtube.com/watch?v=T33NN_pPeNI
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
