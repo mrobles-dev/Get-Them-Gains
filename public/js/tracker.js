f6dfa0e93dd3abe901d2dff3b07c477f677165cd
kevincallaghan <kevincallaghan5@yahoo.com>
2023-05-18 12:38:06 -0500

    Nav animation

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -4,3 +4,19 @@ window.onload = function() {
     delayedImage.classList.add('fade-in');
   }, 300);
 };
+
+// Source: Beyond Fireship Youtube Video "Subtle, yet Beautiful Scroll Animations" link: https://www.youtube.com/watch?v=T33NN_pPeNI
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add('showNav');
+    } else {
+      entry.target.classList.remove('showNav');
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hiddenNav');
+hiddenElements.forEach((el) => observer.observe(el));
