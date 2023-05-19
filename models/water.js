6ccd627ac02fceb6ca7d963a5707ead6d325b742
mrobles-dev <miltonjrobles1996@gmail.com>
2023-05-18 20:05:27 -0500

    capital w

--- models/water.js
+++ models/water.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Water extends Model {}
 
@@ -8,31 +8,31 @@ Water.init(
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     user_id: {
       type: DataTypes.INTEGER,
       //allowNull: false,
       references: {
-        model: 'user',
-        key: 'id',
+        model: "user",
+        key: "id",
       },
     },
     date: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
     },
     ounces: {
       type: DataTypes.INTEGER,
       // allowNull: false
-    }
+    },
   },
   {
     sequelize,
     // timestamps: false,
     // freezeTableName: true,
     // underscored: true,
-    modelName: 'water'
+    modelName: "water",
   }
 );
 
